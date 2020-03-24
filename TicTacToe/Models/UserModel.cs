5d22c98a59ee87d825377f1c5a63f2d44fde5e5f
Joan van Houten <joan.vanhouten@gmail.com>
2020-03-24 23:47:58 +0100

    End of chapter 4

--- TicTacToe/Models/UserModel.cs
+++ TicTacToe/Models/UserModel.cs
@@ -1,19 +1,33 @@
 ﻿namespace TicTacToe.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class UserModel
     {
         #region Public properties
 
-        public string    Email                 { get; set; }
+        [ Required ]
+        [ DataType( DataType.EmailAddress ) ]
+        public string Email { get; set; }
+
         public DateTime? EmailConfirmationDate { get; set; }
-        public string    FirstName             { get; set; }
-        public Guid      Id                    { get; set; }
-        public bool      IsEmailConfirmed      { get; set; }
-        public string    LastName              { get; set; }
-        public string    Password              { get; set; }
-        public int       Score                 { get; set; }
+
+        [ Required ]
+        public string FirstName { get; set; }
+
+        public Guid Id { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        [ Required ]
+        public string LastName { get; set; }
+
+        [ Required() ]
+        [ DataType( DataType.Password) ]
+        public string Password { get; set; }
+
+        public int Score { get; set; }
 
         #endregion
     }
--- TicTacToe/Startup.cs
+++ TicTacToe/Startup.cs
@@ -3,8 +3,12 @@ namespace TicTacToe
     using Extensions;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Rewrite;
+    using Microsoft.AspNetCore.Routing;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Models;
     using Services;
 
     public class Startup
@@ -24,7 +28,41 @@ namespace TicTacToe
             }
 
             app.UseStaticFiles();
+
+            var options = new RewriteOptions().AddRewrite("NewUser", "UserRegistration/Index", false);
+            app.UseRewriter( options );
+            
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapGet( "CreateUser", context =>
+                                               {
+                                                   var firstName = context.Request.Query[ "firstName" ];
+                                                   var lastName  = context.Request.Query[ "lastName" ];
+                                                   var email     = context.Request.Query[ "email" ];
+                                                   var password  = context.Request.Query[ "password" ];
+
+                                                   var userService = context.RequestServices.GetService< IUserService >();
+
+                                                   userService.RegisterUser( new UserModel
+                                                                             {
+                                                                                 FirstName = firstName,
+                                                                                 LastName  = lastName,
+                                                                                 Email     = email,
+                                                                                 Password  = password
+                                                                             } );
+
+                                                   return context.Response.
+                                                       WriteAsync( $"User {firstName} {lastName} has been successfully created." );
+                                               }
+                               );
+            
+            var newUserRoutes = routeBuilder.Build();
+            
+            app.UseRouter( newUserRoutes );
+
             app.UseCommunicationMiddleware();
+            app.UseStatusCodePages( "text/plain", "HTTP Error - Status Code {0}" );
+
             app.UseRouting();
 
             app.UseEndpoints( endpoints =>
@@ -33,6 +71,7 @@ namespace TicTacToe
                                                                name: "default",
                                                                pattern: "{controller=Home}/{action=Index}/{id?}" );
                                   endpoints.MapRazorPages();
+                                  endpoints.MapControllers();
                               } );
         }
 
@@ -43,6 +82,7 @@ namespace TicTacToe
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddSingleton< IUserService, UserService >();
+            services.AddRouting();
         }
 
         #endregion
